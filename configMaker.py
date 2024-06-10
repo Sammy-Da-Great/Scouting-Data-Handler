d7bff825de1e6befed9a8c14a210fd7f93ebc7e0
Sammy-Da-Great <sammyhusma@gmail.com>
2024-06-10 18:46:40 -0500

    Added ability to read config files

--- configMaker.py
+++ configMaker.py
@@ -1,14 +1,22 @@
 import json
+from types import SimpleNamespace
 
-class global_config:
+class Global_Config:
     def __init__(self, host, user, password): 
         self.host = host
         self.user = user
         self.password = password
+    def __str__(self):
+        return "{0}, {1}, {2}".format(self.host, self.user, self.password)
 
 def make_config(config, file_name):
     f = open("config/" + file_name,"w")
     f.write(json.dumps(config.__dict__))
-    f.close
+    f.close()
 
-make_config(global_config("defaultHost", "defaultUser", "defaultPassword"), "global_config.json")
+make_config(Global_Config("defaultHost", "defaultUser", "defaultPassword"), "global_config.json")
+
+def read_global_config(file_name):
+    f = open("config/" + file_name,"r")
+    return(Global_Config(**json.loads(f.read())))
+    f.close()
