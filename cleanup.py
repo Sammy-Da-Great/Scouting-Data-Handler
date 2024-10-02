8a33bca234622d3568a526730eeda68857f2ea0d
Sammy-Da-Great <sammyhusma@gmail.com>
2024-10-02 16:53:50 -0700

    now removes entire tmp/ tree

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+import shutil
 import os
 import glob
-def remove_temp_dir():
-    print("Exited Application")
-    files = glob.glob('tmp/*')
+
+def remove_dir(path):
+    files = glob.glob(path + '*')
     for f in files:
         os.remove(f)
-    Path.rmdir('tmp/')
+    Path.rmdir(path)
+
+def remove_temp_dir():
+    print("Exited Application")
+    shutil.rmtree("tmp/")
