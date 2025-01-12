b12378b261f65138e981f896aba5c783bf356039
Sammy-Da-Great <sammyhusma@gmail.com>
2025-01-12 13:02:18 -0800

    preset handler can now add and delete files

--- ModifyData/ModifyPresetHandler.py
+++ ModifyData/ModifyPresetHandler.py
@@ -1,5 +1,19 @@
 import os
 import subprocess
 
+preset_path = "ModifyData\\ModifyPresets"
+presetpreset = "ModifyData\\ModifyPresetPreset.py"
+
 def openFolder(): 
-    os.startfile("ModifyData\\ModifyPresets")
+    os.startfile(preset_path)
+
+def createFile(name):
+    filepath = f"{preset_path}\\{name}.py"
+
+    with open(presetpreset,'r') as firstfile, open(filepath,'a') as secondfile:
+        for line in firstfile:
+            secondfile.write(line)
+
+def delFile(name):
+    filepath = f"{preset_path}\\{name}.py"
+    os.remove(filepath)
