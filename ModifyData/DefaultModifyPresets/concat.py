a67fc27312cc108d6aca5b1251e27f55be13f542
Sammy-Da-Great <sammyhusma@gmail.com>
2025-06-02 15:05:56 -0700

    concat now can have a separator

--- ModifyData/DefaultModifyPresets/concat.py
+++ ModifyData/DefaultModifyPresets/concat.py
@@ -9,11 +9,13 @@ along with this program. If not, see https://www.gnu.org/licenses/.
 from ModifyData.ModifyPresetPreset import *
 
 data_type = "varchar(45)"
-constants = []
+constants = ["separator"]
 
-def funct(prefix, column):
+def funct(prefix, separator, column):
     if prefix is None:
         prefix = ""
     if column is None:
         column = ""
-    return (prefix + column)
+    if separator is None:
+        separator = ""
+    return (prefix + separator + column)
